988738a4484849010c164c64b3fff626f1c19ff9
msncps <122748630+msncps@users.noreply.github.com>
2023-01-17 22:54:56 +0000

    Update listaccumulate.py

--- listaccumulate.py
+++ listaccumulate.py
@@ -1,19 +1,13 @@
-
 import pandas as pd  
-
-    
 # List1  
-
 lst = [['Jan', 25], ['Feb', 30], 
-
        ['Mar', 26], ['Apr', 22]] 
-
-    
-
 df = pd.DataFrame(lst, columns =['Month', 'Sales'])
-TotalUptoPrevRow = 0
+TotalUptoPrevRow = 0 # to start with cumulative with using 0
+for everyrow in range(len(df)): #for repeating every row
+    TotalUptoPrevRow = TotalUptoPrevRow +  df.loc[everyrow, "Sales"] #actual formula for totalling
+    df.loc[everyrow, "Cum Sales"] = TotalUptoPrevRow #for storing the total
+    df
 
-for row in range(len(df)): 
-    TotalUptoPrevRow = TotalUptoPrevRow +  df.loc[row, "Sales"]
-
-    df.loc[row, "Cum Sales"] = TotalUptoPrevRow
+df['Cumulative Sales'] = df['Sales'].cumsum()
+df
